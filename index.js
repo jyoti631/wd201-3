30e4dd133c4db2d64bcdd4946e384f33e4a22b78
jyoti631 <113690692+jyoti631@users.noreply.github.com>
2023-01-09 14:11:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ todos.add({ title: "Service Vehicle", dueDate: today, completed: false });
 todos.add({ title: "File taxes", dueDate: tomorrow, completed: false });
 todos.add({ title: "Pay electric bill", dueDate: tomorrow, completed: false });
 
-console.log("My Todo-list\n\n");
+console.log("My Todo-list\n");
 
 console.log("Overdue");
 var overdues = todos.overdue();
